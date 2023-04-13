04afbd31545799bb0a64e382667bc03cebf72646
Jocelle Bautista <jcllebautista@gmail.com>
2023-04-13 13:21:03 -0400

    Fetch news article from api

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { fetchData } from '../../api'
+import './Articles.css'
+import { ArticleDetails } from '../ArticleDetails/ArticleDetails';
 
 export const Articles = () => {
-  return (
-    <div className="articles">
-      <h3>Articles</h3>
-    </div>
-  )
-}
+  
+  const [articles, setArticles] = useState([])
+  
+  const fetchArticles = async () => {
+    try {
+      const data = await fetchData()
+      console.log('news', data.results)
+      setArticles(data.results)
+      // setNetworkError(false)
+    } catch (error) {
+      console.error(error)
+      // setNetworkError(true)
+    }
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
+    return (
+      <>
+        <ArticleDetails />
+      </>
+    )
+  }
