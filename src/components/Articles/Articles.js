3a032c3ef1a8c42684cc1a9d387c10a69e30c1e4
Jocelle Bautista <jcllebautista@gmail.com>
2023-04-13 10:23:20 -0400

    Add components

---
+++ src/components/Articles/Articles.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Articles = () => {
+  return (
+    <div className="articles">
+      <h3>Articles</h3>
+    </div>
+  )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <h1>Times' Leak</h1>
+    </div>
+  )
+}
